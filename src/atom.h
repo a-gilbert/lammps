17f01372c4bf8c049a3bee48713fd21f7d0c7853
Austin <agilbert39@gatec.edu>
2018-07-13 00:01:35 -0400

    full steam ahead

--- src/atom.h
+++ src/atom.h
@@ -148,7 +148,7 @@ class Atom : protected Pointers {
   int rmass_flag,radius_flag,omega_flag,torque_flag,angmom_flag;
   int vfrac_flag,spin_flag,eradius_flag,ervel_flag,erforce_flag;
   int cs_flag,csforce_flag,vforce_flag,ervelforce_flag,etag_flag;
-  int rho_flag,e_flag,cv_flag,vest_flag;
+  int rho_flag,e_flag,cv_flag,sph_mu_flag,vest_flag;
   int dpd_flag,edpd_flag,tdpd_flag;
 
   //USER-SPIN package
