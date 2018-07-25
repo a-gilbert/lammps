e25c140e615d2d6a4d9617d3a8fb9e4401c84dca
Austin <agilbert39@gatec.edu>
2018-07-25 13:39:04 -0400

    see

--- src/pair_qsp_kelbg.cpp
+++ src/pair_qsp_kelbg.cpp
@@ -52,7 +52,7 @@ PairQspKelbg::~PairQspKelbg()
 }
 
 double PairQspKelbg::j1(double x, double xi) {
-  double out = exp(-x*(x-1));
+  double out = x*exp(-x*(x-1));
   double t1 = 1-exp(-1*MY_2PI*MY_PIS*xi/x);
   out = out/t1;
   return out;
